5d3bb85e3206d942827b7b93e985728cf4598296
angeleVG <a.vangroesen@outlook.com>
2025-03-20 19:22:25 +0100

    importing json data

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from 'react'
 import NavBar from './components/NavBar'
 import SideBar from './components/SideBar'
 import Footer from './components/Footer'
+import TaskList from './components/TaskList'
+import TaskData from './data/tasks.json'
 import './App.css'
 
 function App() {
+
+
+  
 
   return (
     <>
     <NavBar />
     <SideBar />
-      <Footer />
+    <TaskList />
+    <button>Add</button>
+    <button>Remove</button>
+    <Footer />
   </>
   )
 }
---
+++ src/components/TaskList.jsx
@@ -0,0 +1,9 @@
+function TaskList () {
+    return(
+        <main className="main-list">
+        <h1>Hello Angele</h1>
+        </main>
+    );
+}
+
+export default TaskList
