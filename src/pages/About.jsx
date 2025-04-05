6292525219c6d4ee462df6bfe7bca9bf4e6c2ea4
angeleVG <a.vangroesen@outlook.com>
2025-04-05 18:06:30 +0200

    add team member details on about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,6 +3,16 @@ function About() {
     <div>
       <h1>Todology App</h1>
       <p>A simple To-do app allowing the user to create and manage tasks, as well as check off (or uncheck) the existing tasks when completed.</p>
+  <h2>Santiago Villa:</h2>
+  <ul>
+    <li> https://www.linkedin.com/in/villa-santiago</li>
+    <li>https://github.com/villa-santiago</li>
+  </ul>
+  <h2>Angèle van Groesen:</h2>
+  <ul>
+    <li>https://www.linkedin.com/in/angelevangroesen</li>
+    <li>https://github.com/angeleVG</li>
+  </ul>
     </div>
   );
 }
